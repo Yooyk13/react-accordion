c8cc22b59bf29f2b0c9bda6fbec0a7197c8934bc
yooyk13 <skyagami@gmai.com>
2024-03-29 12:03:52 +0530

    added config file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const faqs = [
+  {
+    id: 1,
+    title: "What is Lorem Ipsum?",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 2,
+    title: "Why do we use it?",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+  },
+  {
+    id: 3,
+    title: "Where does it come from?",
+    description:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" max-w-[300px] sm:max-w-lg mx-auto mt-10 ">
+      {faqs.map((val) => (
+        <Accordion key={val.id} {...val} />
+      ))}
     </div>
   );
 }
 
-export default App;
+function Accordion({ id, title, description }) {
+  const [open, setOpen] = useState(false);
+
+  function handleToggle() {
+    setOpen(!open);
+  }
+
+  return (
+    <div
+      className={`bg-white shadow-2xl  mb-4 ${
+        open ? "border-t-4 border-teal-300" : ""
+      }`}
+    >
+      <div
+        className="flex items-center justify-between p-4 cursor-pointer"
+        onClick={handleToggle}
+      >
+        <p
+          className={`text-lg font-medium ${
+            open ? "text-teal-700" : "text-slate-700"
+          }`}
+        >
+          {title}
+        </p>
+        <span className="text-lg">{open ? "-" : "+"}</span>
+      </div>
+      {open && (
+        <div className="mt-0 p-4 relative top-[-20px]">
+          <p className="text-gray-700">{description}</p>
+        </div>
+      )}
+    </div>
+  );
+}
